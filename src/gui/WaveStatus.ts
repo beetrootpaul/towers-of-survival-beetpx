f5de794a0b45432a3e7a49158f4d4e8123d3df65
Beetroot Paul <paul@beetrootpaul.com>
2023-08-05 19:19:50 +0200

    draw wave progress line

--- src/gui/WaveStatus.ts
+++ src/gui/WaveStatus.ts
@@ -12,8 +12,6 @@ export class WaveStatus {
   draw(): void {
     if (this.#waves.wait) {
       const waveLabel = `wave ${this.#waves.waveNumber}`;
-      // TODO: migrate from Lua
-      //             local progress = #waves.current_wait().progress()
       const waveLabelSize = u.measureTextSize(waveLabel);
       // TODO: migrate from Lua
       //             local progress_width = flr(progress * progress_width_max)
@@ -28,17 +26,16 @@ export class WaveStatus {
         p8c.brownPurple
       );
 
-      // TODO: migrate from Lua
-      //             if progress_width > 0 then
-      //                 line(progress_x, progress_y, progress_x + progress_width - 1, progress_y, a.colors.brown_purple)
-      //             end
+      const progressW = Math.floor(
+        this.#waves.wait.progress() * waveLabelSize.x
+      );
+      if (progressW > 0) {
+        // TODO: make it possible to do `add(x,y)`
+        BeetPx.line(xy1, xy1.add(v_(progressW, 1)), p8c.brownPurple);
+      }
     } else if (this.#waves.wave) {
       const waveLabel = `wave ${this.#waves.waveNumber}`;
-      // TODO: migrate from Lua
-      //             local progress = #waves.current_wave().progress()
       const waveLabelSize = u.measureTextSize(waveLabel);
-      // TODO: migrate from Lua
-      //             local progress_width = flr(progress * progress_width_max)
       const xy1 = v_(
         g.canvasSize.x / 2 - Math.ceil(waveLabelSize.x / 2),
         g.warzoneBorder - 2
@@ -50,16 +47,21 @@ export class WaveStatus {
         p8c.greyLight
       );
 
-      // TODO: migrate from Lua
-      //             local gap = 1
-      //             local remaining_progress_width = progress_width_max - progress_width - gap
-      //
-      //             if progress_width > 0 then
-      //                 line(progress_x, progress_y, progress_x + progress_width - 1, progress_y, a.colors.salmon)
-      //             end
-      //             if remaining_progress_width > 0 then
-      //                 line(progress_x + progress_width_max - remaining_progress_width, progress_y, progress_x + progress_width_max - 1, progress_y, a.colors.brown_purple)
-      //             end
+      const progressW = Math.floor(
+        this.#waves.wave.progress() * waveLabelSize.x
+      );
+      if (progressW > 0) {
+        BeetPx.line(xy1, xy1.add(v_(progressW, 1)), p8c.salmon);
+      }
+
+      const gap = 1;
+      if (waveLabelSize.x - progressW - gap > 0) {
+        BeetPx.line(
+          xy1.add(v_(progressW + gap, 0)),
+          xy1.add(v_(waveLabelSize.x, 1)),
+          p8c.brownPurple
+        );
+      }
     }
   }
 }
